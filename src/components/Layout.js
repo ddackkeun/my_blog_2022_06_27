75e0c1384be76b9a25f9964eb47233a883c7bfc1
SeungKeun Choi <77659341+ddackkeun@users.noreply.github.com>
2022-06-28 17:41:02 +0900

    [기능추가] 블로그 기능 추가 - 블로그 페이지 개발 - 블로그 내용 상세 페이지 연결 추가

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,29 @@
 import React from "react";
-import {Link} from "gatsby"
-import { container, heading, navLinks, navLinkItem, navLinkText } 
+import {Link, useStaticQuery, graphql} from "gatsby"
+import { container, heading, navLinks, navLinkItem, navLinkText, siteTitle, } 
   from "./Layout.module.css";
 
 function Layout({ pageTitle, children}){
+
+  const data = useStaticQuery(graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`);
+
   return(
     <>
       <div className={container}>
-        <title>{pageTitle}</title>
+        <title>
+          {pageTitle} | {data.site.siteMetadata.title}
+        </title>
+        <header className="siteTitle">{data.site.siteMetadata.title}</header>
+
         <nav>
           <ul className={navLinks}>
             <li className={navLinkItem}>
@@ -15,6 +31,9 @@ function Layout({ pageTitle, children}){
             </li>
             <li className={navLinkItem}>
               <Link className={navLinkText} to="/about">About</Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link className={navLinkText} to="/blog">Blog</Link>
             </li>
           </ul>
         </nav>
